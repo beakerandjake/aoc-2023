3618dde88ba30e82a1263f9bde0e42b1add2b81d
beakerandjake <beakerandjake@users.noreply.github.com>
2023-12-02 15:23:35 -0700

    day 2 level 2

--- src/day_02.js
+++ src/day_02.js
@@ -3,39 +3,53 @@
  * Puzzle Description: https://adventofcode.com/2023/day/2
  */
 
-const parseId = (str) => Number(str.match(/Game (\d+)/)[1]);
+import { sum, product } from "./util/array.js";
 
-const parseSet = (str) =>
-  str.split(",").reduce((acc, draw) => {
-    const [, count, color] = draw.match(/(\d+) (red|green|blue)/);
-    return acc.set(color, Number(count));
-  }, new Map());
+/**
+ * Maps a color key to the index of that color in a games color array.
+ */
+const indexes = { red: 0, green: 1, blue: 2 };
 
+/**
+ * Finds and returns the id portion of the games input string.
+ */
+const parseId = (str) => str.match(/Game (\d+)/)[1];
+
+/**
+ * Parses the cubes portion of the input string, returns the max required cubes per color.
+ */
+const parseCubes = (str) =>
+  str.split(";").reduce((acc, handful) => {
+    for (const cube of handful.split(",")) {
+      const [, count, color] = cube.match(/(\d+) (red|green|blue)/);
+      acc[indexes[color]] = Math.max(acc[indexes[color]], count);
+    }
+    return acc;
+  }, Array(3).fill(0));
+
+/**
+ * Parses a single line of input and returns information about that game.
+ */
 const parseLine = (line) => {
-  const [idPart, setsPart] = line.split(":");
-  return { id: parseId(idPart), sets: setsPart.split(";").map(parseSet) };
+  const [id, cubes] = line.split(":");
+  return { id: parseId(id), cubes: parseCubes(cubes) };
 };
 
-const valueOrDefault = (map, key, defaultValue) =>
-  map.has(key) ? map.get(key) : defaultValue;
-
-const isPossible = (set, requirements) =>
-  Object.entries(requirements).every(
-    ([color, requiredCount]) => valueOrDefault(set, color, 0) <= requiredCount
+/**
+ * Returns the solution for level one of this puzzle.
+ */
+export const levelOne = ({ lines }) => {
+  const bag = [12, 13, 14];
+  return sum(
+    lines
+      .map(parseLine)
+      .filter(({ cubes }) => bag.every((x, i) => cubes[i] <= x))
+      .map(({ id }) => Number(id))
   );
+};
 
-const solve = (lines, requirements) =>
-  lines
-    .map(parseLine)
-    .filter(({ sets }) => sets.every((set) => isPossible(set, requirements)))
-    .map(({ id }) => id)
-    .reduce((acc, id) => acc + id, 0);
-
-export const levelOne = ({ lines }) =>
-  solve(lines, {
-    red: 12,
-    green: 13,
-    blue: 14,
-  });
-
-export const levelTwo = ({ input, lines }) => {};
+/**
+ * Returns the solution for level one of this puzzle.
+ */
+export const levelTwo = ({ lines }) =>
+  sum(lines.map(parseLine).map(({ cubes }) => product(cubes)));
--- src/util/array.js
+++ src/util/array.js
@@ -4,3 +4,10 @@
  * @returns {Number}
  */
 export const sum = (items) => items.reduce((acc, x) => acc + x, 0);
+
+/**
+ * Returns the product of each item in the array
+ * @param {Number[]} items
+ * @returns {Number}
+ */
+export const product = (items) => items.reduce((acc, x) => acc * x, 1);
