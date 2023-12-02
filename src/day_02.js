a9ab3853e67558a34dcbdd7e699850a2faa700b4
beakerandjake <beakerandjake@users.noreply.github.com>
2023-12-02 13:49:05 -0700

    day 2 level 1

--- src/day_02.js
+++ src/day_02.js
@@ -3,24 +3,39 @@
  * Puzzle Description: https://adventofcode.com/2023/day/2
  */
 
-/**
- * Returns the solution for level one of this puzzle.
- * @param {Object} args - Provides both raw and split input.
- * @param {String} args.input - The original, unparsed input string.
- * @param {String[]} args.lines - Array containing each line of the input string.
- * @returns {Number|String}
- */
-export const levelOne = ({ input, lines }) => {
-  // your code here
+const parseId = (str) => Number(str.match(/Game (\d+)/)[1]);
+
+const parseSet = (str) =>
+  str.split(",").reduce((acc, draw) => {
+    const [, count, color] = draw.match(/(\d+) (red|green|blue)/);
+    return acc.set(color, Number(count));
+  }, new Map());
+
+const parseLine = (line) => {
+  const [idPart, setsPart] = line.split(":");
+  return { id: parseId(idPart), sets: setsPart.split(";").map(parseSet) };
 };
 
-/**
- * Returns the solution for level two of this puzzle.
- * @param {Object} args - Provides both raw and split input.
- * @param {String} args.input - The original, unparsed input string.
- * @param {String[]} args.lines - Array containing each line of the input string.
- * @returns {Number|String}
- */
-export const levelTwo = ({ input, lines }) => {
-  // your code here
-};
+const valueOrDefault = (map, key, defaultValue) =>
+  map.has(key) ? map.get(key) : defaultValue;
+
+const isPossible = (set, requirements) =>
+  Object.entries(requirements).every(
+    ([color, requiredCount]) => valueOrDefault(set, color, 0) <= requiredCount
+  );
+
+const solve = (lines, requirements) =>
+  lines
+    .map(parseLine)
+    .filter(({ sets }) => sets.every((set) => isPossible(set, requirements)))
+    .map(({ id }) => id)
+    .reduce((acc, id) => acc + id, 0);
+
+export const levelOne = ({ lines }) =>
+  solve(lines, {
+    red: 12,
+    green: 13,
+    blue: 14,
+  });
+
+export const levelTwo = ({ input, lines }) => {};
