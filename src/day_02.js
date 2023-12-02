e2ab6ed5f3a19c4d7995483c05f3d5466c39daa2
beakerandjake <beakerandjake@users.noreply.github.com>
2023-12-02 15:28:23 -0700

    move parsing related functions inside of parseLine

--- src/day_02.js
+++ src/day_02.js
@@ -6,31 +6,25 @@
 import { sum, product } from "./util/array.js";
 
 /**
- * Maps a color key to the index of that color in a games color array.
- */
-const indexes = { red: 0, green: 1, blue: 2 };
-
-/**
- * Finds and returns the id portion of the games input string.
- */
-const parseId = (str) => str.match(/Game (\d+)/)[1];
-
-/**
- * Parses the cubes portion of the input string, returns the max required cubes per color.
- */
-const parseCubes = (str) =>
-  str.split(";").reduce((acc, handful) => {
-    for (const cube of handful.split(",")) {
-      const [, count, color] = cube.match(/(\d+) (red|green|blue)/);
-      acc[indexes[color]] = Math.max(acc[indexes[color]], count);
-    }
-    return acc;
-  }, Array(3).fill(0));
-
-/**
  * Parses a single line of input and returns information about that game.
  */
 const parseLine = (line) => {
+  // maps a color key to the index of that color in a games color array.
+  const indexes = { red: 0, green: 1, blue: 2 };
+
+  // returns the id portion of the games input string.
+  const parseId = (str) => str.match(/Game (\d+)/)[1];
+
+  // parses the cubes portion of the input string, returns the max required cubes per color.
+  const parseCubes = (str) =>
+    str.split(";").reduce((acc, handful) => {
+      for (const cube of handful.split(",")) {
+        const [, count, color] = cube.match(/(\d+) (red|green|blue)/);
+        acc[indexes[color]] = Math.max(acc[indexes[color]], count);
+      }
+      return acc;
+    }, Array(3).fill(0));
+
   const [id, cubes] = line.split(":");
   return { id: parseId(id), cubes: parseCubes(cubes) };
 };
