67f9d30834340e2b5d7565aab011a83e3e1ccca8
beakerandjake <beakerandjake@users.noreply.github.com>
2023-12-04 12:47:49 -0700

    add util fn

--- src/util/array.js
+++ src/util/array.js
@@ -11,3 +11,14 @@ export const sum = (items) => items.reduce((acc, x) => acc + x, 0);
  * @returns {number}
  */
 export const product = (items) => items.reduce((acc, x) => acc * x, 1);
+
+/**
+ * Maps each item in the the array to the number of times that item appears in the array.
+ * @param {Array} items
+ * @return {Map<Any,number>}
+ */
+export const frequencyMap = (items) =>
+  items.reduce(
+    (acc, item) => acc.set(item, (acc.get(item) || 0) + 1),
+    new Map()
+  );
