97e9ff6f82a193cde0c95cdeaaf7b08c5fe1785e
beakerandjake <beakerandjake@users.noreply.github.com>
2023-12-04 13:26:55 -0700

    day 4 level 1

--- src/day_04.js
+++ src/day_04.js
@@ -3,23 +3,29 @@
  * Puzzle Description: https://adventofcode.com/2023/day/4
  */
 
+import { intersection } from "./util/set.js";
+import { parseDelimited } from "./util/string.js";
+import { sum } from "./util/array.js";
+
+const parseLine = (line) =>
+  line
+    .split(":")[1]
+    .split("|")
+    .map((str) => new Set(parseDelimited(str.trim(), /\s+/, Number)));
+
 /**
  * Returns the solution for level one of this puzzle.
- * @param {Object} args - Provides both raw and split input.
- * @param {String} args.input - The original, unparsed input string.
- * @param {String[]} args.lines - Array containing each line of the input string.
- * @returns {Number|String}
  */
-export const levelOne = ({ input, lines }) => {
-  // your code here
-};
+export const levelOne = ({ lines }) =>
+  sum(
+    lines
+      .map(parseLine)
+      .map(([a, b]) => intersection(a, b).size)
+      .map((winning) => (winning >= 2 ? 2 ** (winning - 1) : winning))
+  );
 
 /**
  * Returns the solution for level two of this puzzle.
- * @param {Object} args - Provides both raw and split input.
- * @param {String} args.input - The original, unparsed input string.
- * @param {String[]} args.lines - Array containing each line of the input string.
- * @returns {Number|String}
  */
 export const levelTwo = ({ input, lines }) => {
   // your code here
