bd8acf36e203781bf3d64cfc39339d527736c990
beakerandjake <beakerandjake@users.noreply.github.com>
2023-12-04 15:02:47 -0700

    day 4 level 2

--- src/day_04.js
+++ src/day_04.js
@@ -3,30 +3,40 @@
  * Puzzle Description: https://adventofcode.com/2023/day/4
  */
 
-import { intersection } from "./util/set.js";
-import { parseDelimited } from "./util/string.js";
 import { sum } from "./util/array.js";
+import { repeat } from "./util/functions.js";
+import { intersectionCount } from "./util/set.js";
+import { parseDelimited } from "./util/string.js";
 
-const parseLine = (line) =>
-  line
+/**
+ * Parses the line of input and returns the number of winning numbers.
+ */
+const parseLine = (line) => {
+  const [winners, mine] = line
     .split(":")[1]
     .split("|")
     .map((str) => new Set(parseDelimited(str.trim(), /\s+/, Number)));
+  return intersectionCount(winners, mine);
+};
 
 /**
  * Returns the solution for level one of this puzzle.
  */
-export const levelOne = ({ lines }) =>
-  sum(
-    lines
-      .map(parseLine)
-      .map(([a, b]) => intersection(a, b).size)
-      .map((winning) => (winning >= 2 ? 2 ** (winning - 1) : winning))
-  );
+export const levelOne = ({ lines }) => {
+  const points = (x) => (x >= 2 ? 2 ** (x - 1) : x);
+  return sum(lines.map(parseLine).map(points));
+};
 
 /**
  * Returns the solution for level two of this puzzle.
  */
-export const levelTwo = ({ input, lines }) => {
-  // your code here
+export const levelTwo = ({ lines }) => {
+  const cards = lines.map(parseLine);
+  const copies = Array(cards.length).fill(1);
+  cards.forEach((matchCount, i) =>
+    repeat(matchCount, (x) => {
+      copies[x + i + 1] += copies[i];
+    })
+  );
+  return sum(copies);
 };
