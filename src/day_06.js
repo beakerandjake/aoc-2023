e0aee3054388513a26767397d77d9277f5339dcc
beakerandjake <beakerandjake@users.noreply.github.com>
2023-12-07 16:16:10 -0700

    day 6 level 1 first pass

--- src/day_06.js
+++ src/day_06.js
@@ -3,23 +3,39 @@
  * Puzzle Description: https://adventofcode.com/2023/day/6
  */
 
+import { product, zip } from "./util/array.js";
+import { parseDelimited } from "./util/string.js";
+
 /**
- * Returns the solution for level one of this puzzle.
- * @param {Object} args - Provides both raw and split input.
- * @param {String} args.input - The original, unparsed input string.
- * @param {String[]} args.lines - Array containing each line of the input string.
- * @returns {Number|String}
+ * Parse the input and return the races.
  */
-export const levelOne = ({ input, lines }) => {
-  // your code here
+const parseRaces = (lines) => {
+  const values = (line) =>
+    parseDelimited(line.split(":")[1].trim(), /\s+/, Number);
+  return zip(values(lines[0]), values(lines[1]));
 };
 
 /**
+ * Count the number of ways to beat the record time in the race.
+ */
+const waysToWin = ([raceTime, record]) => {
+  let count = 0;
+  for (let t = 1; t <= raceTime; t++) {
+    if (t * (raceTime - t) > record) {
+      count++;
+    }
+  }
+  return count;
+};
+
+/**
+ * Returns the solution for level one of this puzzle.
+ */
+export const levelOne = ({ lines }) =>
+  product(parseRaces(lines).map(waysToWin));
+
+/**
  * Returns the solution for level two of this puzzle.
- * @param {Object} args - Provides both raw and split input.
- * @param {String} args.input - The original, unparsed input string.
- * @param {String[]} args.lines - Array containing each line of the input string.
- * @returns {Number|String}
  */
 export const levelTwo = ({ input, lines }) => {
   // your code here
