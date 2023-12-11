a8e7933c4a70e83fbbe5d982334f036de190e739
beakerandjake <beakerandjake@users.noreply.github.com>
2023-12-11 10:01:39 -0700

    first pass day 9

--- src/day_09.js
+++ src/day_09.js
@@ -3,24 +3,59 @@
  * Puzzle Description: https://adventofcode.com/2023/day/9
  */
 
+import { parseDelimited } from "./util/string.js";
+import { sum } from "./util/array.js";
+
 /**
- * Returns the solution for level one of this puzzle.
- * @param {Object} args - Provides both raw and split input.
- * @param {String} args.input - The original, unparsed input string.
- * @param {String[]} args.lines - Array containing each line of the input string.
- * @returns {Number|String}
+ * Parses a line of input and returns the history.
  */
-export const levelOne = ({ input, lines }) => {
-  // your code here
+const parseHistory = (line) => parseDelimited(line, " ", Number);
+
+/**
+ * Returns a new array containing the difference at each step of the history.
+ */
+const sequence = (history) => {
+  const result = [];
+  for (let i = 1; i < history.length; i++) {
+    result.push(history[i] - history[i - 1]);
+  }
+  return result;
 };
 
 /**
- * Returns the solution for level two of this puzzle.
- * @param {Object} args - Provides both raw and split input.
- * @param {String} args.input - The original, unparsed input string.
- * @param {String[]} args.lines - Array containing each line of the input string.
- * @returns {Number|String}
+ * Returns the prediction of the next value of the history based on its sequences.
  */
-export const levelTwo = ({ input, lines }) => {
-  // your code here
+const extrapolate = (sequences) => {
+  let nextValue = 0;
+  for (let i = 1; i < sequences.length; i++) {
+    nextValue += sequences[i].at(-1);
+  }
+  return nextValue;
 };
+
+/**
+ * Returns the prediction of the next value.
+ */
+const nextValue = (history) => {
+  const sequences = [history];
+  while (sequences.at(-1)?.some((x) => x !== 0)) {
+    sequences.push(sequence(sequences.at(-1)));
+  }
+  return extrapolate(sequences.reverse());
+};
+
+/**
+ * Returns the sum of the next value of each history.
+ */
+const solve = (histories) => sum(histories.map(nextValue));
+
+/**
+ * Returns the solution for level one of this puzzle.
+ */
+export const levelOne = ({ lines }) => solve(lines.map(parseHistory));
+
+/**
+ * Returns the solution for level two of this puzzle.
+ */
+export const levelTwo = ({ lines }) =>
+  solve(lines.map((line) => parseHistory(line).reverse()));
