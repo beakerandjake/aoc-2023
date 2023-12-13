7bf00430c45bb3e93850271b4873b7f5513cf97a
beakerandjake <beakerandjake@users.noreply.github.com>
2023-12-13 16:37:22 -0700

    clean up, use FlatArray

--- src/day_10.js
+++ src/day_10.js
@@ -3,8 +3,8 @@
  * Puzzle Description: https://adventofcode.com/2023/day/10
  */
 
+import { FlatArray } from "./util/flatArray.js";
 import { cardinalNeighbors } from "./util/neighbors.js";
-import { Vector2 } from "./util/vector2.js";
 
 /**
  * Maps each pipe tile to neighbors which it can connect it.
@@ -19,76 +19,55 @@ const pipeConnections = new Map([
 ]);
 
 /**
- * Parse the input and returns the world.
+ * Parse the input and return the world.
  */
-const parseWorld = (lines) => ({
-  items: lines,
-  shape: { height: lines.length, width: lines[0].length },
-});
-
-const inBounds = ({ height, width }, { y, x }) =>
-  y >= 0 && y < height && x >= 0 && x < width;
-
-const findStart = ({ items, shape }) => {
-  // return the position of the "S" tile.
-  const findPosition = () => {
-    for (let y = 0; y < shape.height; y++) {
-      for (let x = 0; x < shape.width; x++) {
-        if (items[y][x] === "S") {
-          return new Vector2(y, x);
+const parseWorld = (lines) => {
+  // guess the type of pipe the start tile based off of its neighbors.
+  const guessStartTile = (world, startPosition) => {
+    for (const [tile, connections] of pipeConnections.entries()) {
+      let connectedCount = 0;
+      for (const [pos, dir] of cardinalNeighbors(startPosition)) {
+        const neighbor = world.inBounds(pos) ? world.elementAt(pos) : null;
+        if (neighbor && connections[dir]?.has(neighbor)) {
+          connectedCount++;
         }
       }
-    }
-    throw new Error("Could not find start location");
-  };
-
-  // return the type of pipe of the "S" tile.
-  const findTile = (position) => {
-    // test every tile type and return the first tile which is connected to two neighbors.
-    for (const [tile, connections] of pipeConnections.entries()) {
-      const connected = [...cardinalNeighbors(position)].filter(
-        ([pos, dir]) => {
-          const neighbor = inBounds(shape, pos) ? items[pos.y][pos.x] : null;
-          return neighbor && connections[dir]?.has(neighbor);
-        }
-      );
-      if (connected.length === 2) {
+      if (connectedCount === 2) {
         return tile;
       }
     }
-    throw new Error("Could not find start tile type");
+    throw new Error("Could not determine start tile type.");
   };
-  const position = findPosition();
-  return { position, tile: findTile(position) };
+
+  const world = new FlatArray(lines);
+  const startIndex = world.findIndex((x) => x === "S");
+  const startPosition = world.indexToPosition(startIndex);
+  // "fix" the input by replacing the state tile "S" with it's actual pipe character.
+  world[startIndex] = guessStartTile(world, startPosition);
+  return { world, startPosition };
 };
 
 /**
- *
+ * Explore the entire loop from the start location.
  */
-const bfs = (world, visitFn) => {
+const bfs = (world, startPosition, visitFn) => {
   const visited = new Set();
-  const queue = [{ ...findStart(world), distance: 0 }];
+  const queue = [{ position: startPosition, distance: 0 }];
   while (queue.length) {
     const current = queue.shift();
+    // skip node if visited.
     if (!visited.has(current.position.toString())) {
-      const { position, tile, distance } = current;
-
-      // mark current as visited.
+      const { position, distance } = current;
+      // visit current.
       visited.add(position.toString());
-      if (visitFn(current) === true) {
-        break;
-      }
-
+      visitFn(current);
       // visit each neighbor
+      const currentTile = world.elementAt(position);
       for (const [neighbor, direction] of cardinalNeighbors(position)) {
-        if (inBounds(world.shape, neighbor)) {
-          const neighborTile = world.items[neighbor.y][neighbor.x];
-          if (pipeConnections.get(tile)[direction]?.has(neighborTile)) {
-            queue.push({
-              position: neighbor,
-              tile: neighborTile,
-              distance: distance + 1,
-            });
+        if (world.inBounds(neighbor)) {
+          const neighborTile = world.elementAt(neighbor);
+          if (pipeConnections.get(currentTile)[direction]?.has(neighborTile)) {
+            queue.push({ position: neighbor, distance: distance + 1 });
           }
         }
       }
@@ -101,9 +80,9 @@ const bfs = (world, visitFn) => {
  * Returns the solution for level one of this puzzle.
  */
 export const levelOne = ({ lines }) => {
-  const world = parseWorld(lines);
+  const { world, startPosition } = parseWorld(lines);
   let maxDistance = 0;
-  bfs(world, ({ distance }) => {
+  bfs(world, startPosition, ({ distance }) => {
     maxDistance = Math.max(distance, maxDistance);
   });
   return maxDistance;
