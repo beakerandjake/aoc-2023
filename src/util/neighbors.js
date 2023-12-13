619313eabafb4be4fbb45ed1f1347b4b05f06a70
beakerandjake <beakerandjake@users.noreply.github.com>
2023-12-13 15:01:45 -0700

    update comments

--- src/util/neighbors.js
+++ src/util/neighbors.js
@@ -25,6 +25,7 @@ export const mooreNeighborhood = [
  * Returns an iterator which loops over the neighbors of the position, yielding each neighbors position.
  * @param {Vector2} position - The point whose neighbors to iterate.
  * @param {Vector2[]} neighborDeltas - The deltas which lead to each neighbor.
+ * @returns {Iterator<Vector2>} A new iterator that yields a position for each cardinal direction.
  */
 export function* neighbors(position, neighborDeltas) {
   for (const delta of neighborDeltas) {
@@ -35,6 +36,7 @@ export function* neighbors(position, neighborDeltas) {
 /**
  * Returns an iterator which loops over the cardinal neighbors of the position, yielding each neighbor and its direction.
  * @param {Vector2} position
+ * @returns {Iterator<[Vector2, string]>} A new iterator that yields [position, direction] pairs for each cardinal direction.
  */
 export function* cardinalNeighbors(position) {
   yield [add(position, up), "north"];
